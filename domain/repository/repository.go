25f16490ea89ca1d804c94d92930f619b6b68ea6
tomocy <tomocy.dev@gmail.com>
2019-07-31 22:15:53 +0900

    fix

--- domain/repository/repository.go
+++ domain/repository/repository.go
@@ -3,5 +3,5 @@ package repository
 import "github.com/tomocy/wakame/domain/model"
 
 type ContributorRepository interface {
-	FetchContributors(owner, repo string, page int) []*model.Contributor
+	FetchContributors(owner, repo string, page int) ([]*model.Contributor, error)
 }
