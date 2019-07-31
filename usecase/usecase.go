84aca52d22c97377afe070946b6ff3899424a2c2
tomocy <tomocy.dev@gmail.com>
2019-07-31 22:25:50 +0900

    add method for usecasse for contributors to fetch a contritubor

--- usecase/usecase.go
+++ usecase/usecase.go
@@ -1,7 +1,39 @@
 package usecase
 
-import "github.com/tomocy/wakame/domain/repository"
+import (
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/tomocy/wakame/domain/model"
+	"github.com/tomocy/wakame/domain/repository"
+)
 
 type ContributorUsecase struct {
 	repo repository.ContributorRepository
+}
+
+func (u *ContributorUsecase) Fetch(owner, repo, uname string) (*model.Contributor, error) {
+	for page := 1; ; page++ {
+		fetcheds, err := u.repo.FetchContributors(owner, repo, page)
+		if err != nil {
+			return nil, report("fetch contributors", err)
+		}
+		if len(fetcheds) <= 0 {
+			break
+		}
+
+		found, err := (model.Contributors(fetcheds)).Find(uname)
+		if err == nil {
+			return found, nil
+		}
+
+		time.Sleep(1 * time.Second)
+	}
+
+	return nil, errors.New("no such contributor")
+}
+
+func report(did string, err error) error {
+	return fmt.Errorf("failed to %s: %s", did, err)
 }
