ae971f4ebe3d89b2fd04b8b43de429ccd88f4db7
tomocy <tomocy.dev@gmail.com>
2019-07-31 22:13:03 +0900

    add interface to represent a contributor repository

---
+++ domain/repository/repository.go
@@ -0,0 +1,7 @@
+package repository
+
+import "github.com/tomocy/wakame/domain/model"
+
+type ContributorRepository interface {
+	FetchContributors(owner, repo string, page int) []*model.Contributor
+}
