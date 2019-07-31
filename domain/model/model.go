16cad8d53c3e6aa794c10d608f021dd6608605a7
tomocy <tomocy.dev@gmail.com>
2019-07-31 22:22:54 +0900

    add method for sets of contributors to find a contributor with given name

--- domain/model/model.go
+++ domain/model/model.go
@@ -1,6 +1,18 @@
 package model
 
+import "errors"
+
 type Contributors []*Contributor
+
+func (cs Contributors) Find(name string) (*Contributor, error) {
+	for _, c := range cs {
+		if c.Name == name {
+			return c, nil
+		}
+	}
+
+	return nil, errors.New("no such contributor")
+}
 
 type Contributor struct {
 	Name          string
