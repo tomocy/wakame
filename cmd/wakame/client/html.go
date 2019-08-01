9924ce25b5288e0e2de6f6d29928cf1c026f714f
tomocy <tomocy.dev@gmail.com>
2019-08-01 23:08:17 +0900

    add method for html to load static html files

--- cmd/wakame/client/html.go
+++ cmd/wakame/client/html.go
@@ -1,9 +1,31 @@
 package client
 
 import (
+	"os"
+	"path/filepath"
+
 	"github.com/tomocy/caster"
 )
 
+func NewHTML() *HTML {
+	return new(HTML)
+}
+
 type HTML struct {
 	caster caster.Caster
+}
+
+func (h *HTML) load() error {
+	var err error
+	h.caster, err = caster.New(&caster.TemplateSet{
+		Filenames: []string{h.join("master.html")},
+	})
+
+	return err
+}
+
+func (h *HTML) join(ss ...string) string {
+	dir := filepath.Join(os.Getenv("GOPATH"), "src/github.com/tomocy/wakame/cmd/wakame/client/resource/html")
+	ps := append([]string{dir}, ss...)
+	return filepath.Join(ps...)
 }
